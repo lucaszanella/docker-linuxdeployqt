41a73d6f8bb6650453da431afb82c7eddcf611f8
Lucas Zanella <me@lucaszanella.com>
2020-06-13 16:48:22 -0300

    fixed seamlyme

--- make_seamly2d_appimage.sh
+++ make_seamly2d_appimage.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p /home/runner/local
-cd /home/project
-qmake PREFIX=/home/runner/local Seamly2D.pro -r CONFIG+=no_ccache CONFIG+=noDebugSymbols
-make
-make install
-
-mkdir -p /home/runner/local/share/applications /home/runner/local/share/icons/hicolor/256x256
-cp dist/seamly2d.desktop /home/runner/local/share/applications/
-cp share/img/Seamly2D_logo_254x254.png /home/runner/local/share/icons/hicolor/256x256/seamly2d.png
-#cp share/img/Seamly2D_logo_254x254.png /home/runner/local/share/icons/hicolor/256x256/seamlyme.png
-#rm /home/runner/local/share/applications/seamly2d.desktop
-#cp dist/seamlyme.desktop /home/runner/local/share/applications
-source /builder.sh
+cp -R /app/usr/lib*/**/* /usr/lib/
+/usr/bin/linuxdeployqt /app/usr/share/applications/seamly2d.desktop -executable=/app/usr/bin/seamlyme -appimage && \
+cp *.AppImage /app/usr
--- make_seamlyme_appimage.sh
+++ make_seamlyme_appimage.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p /home/runner/local
-cd /home/project
-qmake PREFIX=/home/runner/local Seamly2D.pro -r CONFIG+=no_ccache CONFIG+=noDebugSymbols
-make
-make install
-
-mkdir -p /home/runner/local/share/applications /home/runner/local/share/icons/hicolor/256x256
-cp share/img/Seamly2D_logo_254x254.png /home/runner/local/share/icons/hicolor/256x256/seamlyme.png
-cp dist/seamlyme.desktop /home/runner/local/share/applications
-
-source /builder.sh
+cp -R /app/usr/lib*/**/* /usr/lib/
+/usr/bin/linuxdeployqt /app/usr/share/applications/seamlyme.desktop -appimage && \
+cp *.AppImage /app/usr
