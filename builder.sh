0bdafab9560b91f8d7972dbc11899c1262321095
Lucas Zanella <me@lucaszanella.com>
2020-06-15 13:12:26 -0300

    fixed some things

--- builder.sh
+++ builder.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+echo "/usr/bin/linuxdeployqt /app/usr/share/applications/*.desktop $1 -appimage &&"
 cp -R /app/usr/lib*/**/* /usr/lib/
 /usr/bin/linuxdeployqt /app/usr/share/applications/*.desktop $1 -appimage && \
 cp *.AppImage /app/usr
